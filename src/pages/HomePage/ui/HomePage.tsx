4edcb10e48c047775e18d72ebb85ee63dbc49163
KMA <kamil.mardanov04@yandex.ru>
2024-09-23 17:53:11 +0300

    update: home page

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,5 +1,14 @@
+import coolCat from "@/shared/assets/gif/cat-meme.gif";
+import Title from "antd/es/typography/Title";
+import Image from "next/image";
+
 import css from "./HomePage.module.scss";
 
 export const HomePage = () => {
-  return <div className={css.page}>HomePage</div>;
+  return (
+    <div className={css.page}>
+      <Title>Welcome!</Title>
+      <Image src={coolCat} alt="Cool Cat" />
+    </div>
+  );
 };
